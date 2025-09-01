23645295d5eed614bac11ee70106a394184156f9
David <david.samah@student.moringaschool.com>
2025-09-01 08:35:30 +0300

    added play html

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+
+function Play(){
+    alert('You sure?')
+    window.location.href='play.html';
+    
+}
+
+
+function Options(){
+    
+   document.getElementById('Options').innertext="0";
+    
+}
+
+
+function Help(){
+    alert('You sure?')
+    
+}
+
+function Quit(){
+    alert('You sure?')
+    
+}
+
